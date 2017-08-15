cfe29913a5b295cc586dfc7ddcd63d103a820926
gk0909c <gk0909c@gmail.com>
2017-08-15 15:36:09 +0900

    add file loop

--- lib/chapex/cli.rb
+++ lib/chapex/cli.rb
@@ -6,23 +6,32 @@ module Chapex
   class Cli
     # check apex
     def run
-      # create source object
-      filepath = "#{Dir.pwd}/test/fixtures/Apex.cls"
+      file_pattern = 'test/fixtures/**/*.cls'
+      files = Dir.glob("#{Dir.pwd}/#{file_pattern}")
+      reporter = Chapex::Reporter::StdOut.new
+
+      files.each do |f|
+        result = investigate_file(f)
+        reporter.add_violations(f, result)
+      end
+
+      # output
+      reporter.output
+    end
+
+    private
+
+    def investigate_file(filepath)
       source = Source.new(filepath)
 
       # parse source
       parser = Chapex::Parser::Apex.new(source)
       parser.parse
-      puts source.ast
+      # puts source.ast
 
       # check!
       mediator = Mediator.new(source)
-      result = mediator.investigate
-
-      # output
-      reporter = Chapex::Reporter::StdOut.new
-      reporter.add_violations(filepath, result)
-      reporter.output
+      mediator.investigate
     end
   end
 end
